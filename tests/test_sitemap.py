7ce5c9d1b874e6e19be51fafedb62420b067e58b
Iacopo Spalletti <i.spalletti@nephila.it>
2014-08-26 08:18:38 +0200

    Add tests

---
+++ tests/test_sitemap.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from django.utils.timezone import now
+from django.core.cache import cache
+from djangocms_page_sitemap.models import PageSitemapProperties
+from djangocms_page_sitemap.sitemap import ExtendedSitemap
+from djangocms_page_sitemap.utils import get_cache_key
+
+from . import BaseTest
+
+
+class SitemapTest(BaseTest):
+
+    def test_sitemap_base(self):
+        test_string = '<url><loc>http://example.com/it/</loc><lastmod>%s</lastmod><changefreq>monthly</changefreq><priority>0.5</priority></url>' % now().strftime('%Y-%m-%d')
+        self.get_pages()
+        sitemap = self.client.get('/sitemap.xml')
+        self.assertContains(sitemap, test_string)
+
+    def test_sitemap_extended(self):
+        test_string = '<url><loc>http://example.com/it/</loc><lastmod>%s</lastmod><changefreq>never</changefreq><priority>0.2</priority></url>' % now().strftime('%Y-%m-%d')
+        page1, page2 = self.get_pages()
+        page_ext = PageSitemapProperties.objects.create(
+            extended_object=page1, priority='0.2', changefreq='never')
+        page1.publish('it')
+        sitemap = self.client.get('/sitemap.xml')
+        self.assertContains(sitemap, test_string)
+
+    def test_sitemap_cache(self):
+        page1, page2 = self.get_pages()
+        page_ext = PageSitemapProperties.objects.create(
+            extended_object=page1, priority='0.2', changefreq='never')
+        page_ext_2 = PageSitemapProperties.objects.create(
+            extended_object=page2, priority='0.8', changefreq='hourly')
+        page1.publish('it')
+        page1 = page1.get_public_object()
+        page2.publish('it')
+        page2 = page2.get_public_object()
+        sitemap = ExtendedSitemap()
+        self.assertEqual(len(sitemap.items()), 4)
+        for item in sitemap.items():
+            if item.page.pk == page1.pk:
+                self.assertEqual(sitemap.changefreq(item), 'never')
+                ext_key = get_cache_key(item.page)
+                self.assertEqual(cache.get(ext_key), item.page.pagesitemapproperties)
+            if item.page.pk == page2.pk:
+                self.assertEqual(sitemap.changefreq(item), 'hourly')
+
+        ext_key = get_cache_key(page1)
+        page1.pagesitemapproperties.save()
+        self.assertEqual(cache.get(ext_key), None)
+
+        ext_key = get_cache_key(page2)
+        page2.delete()
+        self.assertEqual(cache.get(ext_key), None)
---
+++ tests/test_toolbar.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+from cms.toolbar.items import ModalItem, Menu
+from django.contrib.auth.models import Permission, User
+from django.core.urlresolvers import reverse
+from django.test.utils import override_settings
+from django.utils.encoding import force_text
+from django.utils.translation import ugettext_lazy as _
+
+from djangocms_page_sitemap.cms_toolbar import PAGE_SITEMAP_MENU_TITLE
+from djangocms_page_sitemap.models import PageSitemapProperties
+
+from . import BaseTest
+
+
+class ToolbarTest(BaseTest):
+
+    def test_no_page(self):
+        """
+        Test that no page menu is present if request not in a page
+        """
+        from cms.toolbar.toolbar import CMSToolbar
+        request = self.get_page_request(None, self.user, '/', edit=True)
+        toolbar = CMSToolbar(request)
+        toolbar.get_left_items()
+        page_menu = toolbar.find_items(Menu, name='Page')
+        self.assertEqual(page_menu, [])
+
+    def test_no_perm(self):
+        """
+        Test that no page menu is present if user has no perm
+        """
+        from cms.toolbar.toolbar import CMSToolbar
+        page1, page2 = self.get_pages()
+        request = self.get_page_request(page1, self.user_staff, '/', edit=True)
+        toolbar = CMSToolbar(request)
+        toolbar.get_left_items()
+        page_menu = toolbar.find_items(Menu, name='Page')
+        self.assertEqual(page_menu, [])
+
+    def test_perm(self):
+        """
+        Test that page meta menu is present if user has Page.change_perm
+        """
+        from cms.toolbar.toolbar import CMSToolbar
+        page1, page2 = self.get_pages()
+        self.user_staff.user_permissions.add(Permission.objects.get(codename='change_page'))
+        self.user_staff = User.objects.get(pk=self.user_staff.pk)
+        request = self.get_page_request(page1, self.user_staff, '/', edit=True)
+        toolbar = CMSToolbar(request)
+        toolbar.get_left_items()
+        page_menu = toolbar.menus['page']
+        self.assertEqual(len(page_menu.find_items(ModalItem, name="%s ..." % force_text(PAGE_SITEMAP_MENU_TITLE))), 1)
+
+    @override_settings(CMS_PERMISSION=True)
+    def test_perm_permissions(self):
+        """
+        Test that no page menu is present if user has general page Page.change_perm  but not permission on current page
+        """
+        from cms.toolbar.toolbar import CMSToolbar
+        page1, page2 = self.get_pages()
+        self.user_staff.user_permissions.add(Permission.objects.get(codename='change_page'))
+        self.user_staff = User.objects.get(pk=self.user_staff.pk)
+        request = self.get_page_request(page1, self.user_staff, '/', edit=True)
+        toolbar = CMSToolbar(request)
+        toolbar.get_left_items()
+        page_menu = toolbar.find_items(Menu, name='Page')
+        self.assertEqual(page_menu, [])
+
+    def test_toolbar(self):
+        """
+        Test that PageSitemapProperties item is present for superuser
+        """
+        from cms.toolbar.toolbar import CMSToolbar
+        page1, page2 = self.get_pages()
+        request = self.get_page_request(page1, self.user, '/', edit=True)
+        toolbar = CMSToolbar(request)
+        toolbar.get_left_items()
+        page_menu = toolbar.menus['page']
+        self.assertEqual(len(page_menu.find_items(ModalItem, name="%s ..." % force_text(PAGE_SITEMAP_MENU_TITLE))), 1)
+
+    def test_toolbar_with_items(self):
+        """
+        Test that PageSitemapProperties item is present for superuser if PageSitemapProperties exists for current page
+        """
+        from cms.toolbar.toolbar import CMSToolbar
+        page1, page2 = self.get_pages()
+        page_ext = PageSitemapProperties.objects.create(
+            extended_object=page1, priority='0.2', changefreq='never')
+        request = self.get_page_request(page1, self.user, '/', edit=True)
+        toolbar = CMSToolbar(request)
+        toolbar.get_left_items()
+        page_menu = toolbar.menus['page']
+        meta_menu = page_menu.find_items(ModalItem, name="%s ..." % force_text(PAGE_SITEMAP_MENU_TITLE))[0].item
+        self.assertTrue(meta_menu.url.startswith(reverse('admin:djangocms_page_sitemap_pagesitemapproperties_change', args=(page_ext.pk,))))
+        self.assertEqual(force_text(page_ext), force_text(_(u'Sitemap values for Page %s') % page1.pk))
