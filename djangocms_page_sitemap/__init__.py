e6d68b7165fc96b0e1f69f6b272d5f8cc1470802
Iacopo Spalletti <i.spalletti@nephila.it>
2018-04-07 20:36:29 +0200

    Bump development version [ci skip]

--- djangocms_page_sitemap/__init__.py
+++ djangocms_page_sitemap/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-__version__ = '0.5.2'  # pragma: no cover
+__version__ = '0.5.2.post1'  # pragma: no cover
 __author__ = 'Iacopo Spalletti <i.spalletti@nephila.it>'  # pragma: no cover
