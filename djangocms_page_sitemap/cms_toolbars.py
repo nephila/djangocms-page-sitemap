607ec13d8c76bc0fa4b51084e37c2d9ec8b1b6c8
vipulnarang95 <61502917+vipulnarang95@users.noreply.github.com>
2020-03-04 15:41:25 +0530

    Update compatible with Django CMS version4
    sitemap.py and cms_toolbars.py updated to support page menu and sitemap compatible with Django CMS version 4

--- djangocms_page_sitemap/cms_toolbars.py
+++ djangocms_page_sitemap/cms_toolbars.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-from cms.api import get_page_draft
-from cms.cms_toolbars import PAGE_MENU_THIRD_BREAK
-from cms.toolbar.items import Break
-from cms.toolbar_base import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
 from cms.utils.conf import get_cms_setting
 from cms.utils.permissions import has_page_permission
 from django.urls import NoReverseMatch, reverse
 from django.utils.translation import ugettext_lazy as _
+from cms.cms_toolbars import PlaceholderToolbar
 
 from .models import PageSitemapProperties
 
@@ -17,10 +14,10 @@ PAGE_SITEMAP_MENU_TITLE = _('Sitemap properties')
 
 
 @toolbar_pool.register
-class PageSitemapPropertiesMeta(CMSToolbar):
+class PageSitemapPropertiesMeta(PlaceholderToolbar):
     def populate(self):
         # always use draft if we have a page
-        self.page = get_page_draft(self.request.current_page)
+        self.page = self.request.current_page
         if not self.page:
             # Nothing to do
             return
@@ -44,8 +41,6 @@ class PageSitemapPropertiesMeta(CMSToolbar):
             except AttributeError:
                 not_edit_mode = not self.toolbar.edit_mode_active
             current_page_menu = self.toolbar.get_or_create_menu('page')
-            position = current_page_menu.find_first(
-                Break, identifier=PAGE_MENU_THIRD_BREAK) - 1
             # Page tags
             try:
                 page_extension = PageSitemapProperties.objects.get(extended_object_id=self.page.pk)
@@ -64,5 +59,5 @@ class PageSitemapPropertiesMeta(CMSToolbar):
                 pass
             else:
                 current_page_menu.add_modal_item(
-                    PAGE_SITEMAP_MENU_TITLE, url=url, disabled=not_edit_mode, position=position
+                    PAGE_SITEMAP_MENU_TITLE, url=url, disabled=not_edit_mode
                 )
--- djangocms_page_sitemap/sitemap.py
+++ djangocms_page_sitemap/sitemap.py
@@ -3,10 +3,15 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from cms.sitemaps import CMSSitemap
 from django.core.cache import cache
+from collections import defaultdict
 
 from .models import PageSitemapProperties
 from .settings import PAGE_SITEMAP_CACHE_DURATION, PAGE_SITEMAP_DEFAULT_CHANGEFREQ
 from .utils import get_cache_key
+
+from cms.models import PageContent, PageUrl
+from cms.utils import get_current_site
+from cms.utils.i18n import get_public_languages
 
 
 class ExtendedSitemap(CMSSitemap):
@@ -14,9 +19,39 @@ class ExtendedSitemap(CMSSitemap):
     default_priority = CMSSitemap.priority
 
     def items(self):
-        return super(ExtendedSitemap, self).items().exclude(
-            page__pagesitemapproperties__include_in_sitemap=False
+        # This method was created from this commit:
+        # https://github.com/divio/django-cms/blob/2894ae8bcf92092d947a097499c01ab2bbb0e6df/cms/sitemaps/cms_sitemap.py
+        site = get_current_site()
+        languages = get_public_languages(site_id=site.pk)
+        all_urls = (
+            PageUrl
+            .objects
+            .get_for_site(site)
+            .select_related('page')
+            .filter(language__in=languages, path__isnull=False, page__login_required=False)
+            .order_by('page__node__path')
         )
+        excluded_titles_by_page = defaultdict(set)
+        # Added filter to add pages to excluded translation that have include_in_sitemap as False
+        excluded_translations = (
+            PageContent
+            .objects
+            .filter(language__in=languages, page__node__site=site, page__pagesitemapproperties__include_in_sitemap=False)
+            .values_list('page', 'language')
+        )
+
+        for page_id, language in excluded_translations:
+            excluded_titles_by_page[page_id].add(language)
+
+        valid_urls = []
+
+        for page_url in all_urls:
+            excluded = excluded_titles_by_page.get(page_url.page_id, [])
+
+            if page_url.language in excluded:
+                continue
+            valid_urls.append(page_url)
+        return valid_urls
 
     def priority(self, title):
         ext_key = get_cache_key(title.page)
