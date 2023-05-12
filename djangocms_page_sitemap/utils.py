55dbdac77f6f52344ef5da4aa6e38de4e59740b0
Iacopo Spalletti <i.spalletti@nephila.it>
2023-05-12 23:45:04 +0200

    Fix coverage

--- djangocms_page_sitemap/utils.py
+++ djangocms_page_sitemap/utils.py
@@ -6,10 +6,7 @@ def get_cache_key(page):
     """
     Create the cache key for the current page and language
     """
-    try:
-        site_id = page.node.site_id
-    except AttributeError:
-        site_id = page.site_id
+    site_id = page.node.site_id
     return _get_cache_key("page_sitemap", page, "default", site_id)
 
 
@@ -21,7 +18,7 @@ def is_versioning_enabled():
         try:
             app_config = apps.get_app_config("djangocms_versioning")
             return app_config.cms_extension.is_content_model_versioned(PageContent)
-        except LookupError:
+        except LookupError:  # pragma: no cover
             return False
     except ImportError:
         return False
